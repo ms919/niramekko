500217b365bea687aae2426f47dd1a7a1204dcc1
ms919 <sakuraimiki919@gmail.com>
2021-10-23 13:56:03 +0900

    Add: topページ、modeSelectページ

---
+++ front/layouts/error.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="error-wrapper">
+		<p>Error：{{ error.statusCode }}</p>
+		<p>{{ error.message }}</p>
+	</div>
+</template>
+<script>
+export default {
+	props: {
+		error: {
+			type: Object,
+			default: null,
+		},
+	},
+};
+</script>
+<style scoped>
+.error-wrapper {
+	text-align: center;
+	margin-top: 10rem;
+}
+</style>
--- front/pages/index.vue
+++ front/pages/index.vue
@@ -1,7 +1,7 @@
 <template>
-  <Tutorial/>
+	<div class="text-center">
+		<h1>笑って、笑わせて</h1>
+		<h2>Social Niramekko Service</h2>
+		<NuxtLink to="/modeSelect" class="button btn-pink">PLAY</NuxtLink>
+	</div>
 </template>
-
-<script>
-export default {}
-</script>
