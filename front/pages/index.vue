0b3eea4acc6a280c13428bf1d621dd86143bc399
ms919 <sakuraimiki919@gmail.com>
2021-10-31 17:03:42 +0900

    submodule化

--- front/pages/index.vue
+++
@@ -1,7 +0,0 @@
-<template>
-	<div class="text-center">
-		<h1>笑って、笑わせて</h1>
-		<h2>Social Niramekko Service</h2>
-		<NuxtLink to="/modeSelect" class="button btn-pink">PLAY</NuxtLink>
-	</div>
-</template>
